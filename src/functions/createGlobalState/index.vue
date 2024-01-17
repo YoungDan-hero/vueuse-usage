ef2a55cb7d9b36a327fa102642baeb18dbbbfa8a
YoungDan-hero <1292867089@qq.com>
2024-01-17 15:31:35 +0800

    feat: init project

--- src/functions/createGlobalState/index.vue
+++ src/functions/createGlobalState/index.vue
@@ -4,9 +4,10 @@ import Usage2CreateGlobalState from "./Usage2.vue";
 </script>
 
 <template>
-  createGlobalState
+  <div>createGlobalState</div>
+  <div>----------------------------</div>
   <UsageCreateGlobalState />
+  <div>----------------------------</div>
   <Usage2CreateGlobalState />
 </template>
-
 <style scoped></style>
