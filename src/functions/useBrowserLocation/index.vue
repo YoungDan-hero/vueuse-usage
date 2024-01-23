56fc26c44938c2cfc585a733c92ffc870f280c57
YoungDan-hero <1292867089@qq.com>
2024-01-23 11:14:45 +0800

    feat: add des

--- src/functions/useBrowserLocation/index.vue
+++ src/functions/useBrowserLocation/index.vue
@@ -1,4 +1,11 @@
 <script setup>
+/*
+useBrowserLocation是一个自定义钩子，用于读取和响应浏览器的位置信息。它提供了一个反应性的方式来访问window.location对象，包括URL、查询参数、哈希值等。
+
+useBrowserLocation返回一个反应性的对象，该对象包含了window.location的所有属性，如href、protocol、host、hostname、port、pathname、search和hash等。当浏览器的URL变化时，这个对象会自动更新。
+
+这个钩子在需要根据URL或其部分来改变组件行为的场景中非常有用。例如，你可能希望根据URL的查询参数来过滤列表，或者根据URL的哈希值来滚动到特定的页面部分。
+*/
 import { useBrowserLocation } from "@vueuse/core";
 const location = useBrowserLocation();
 </script>
--- src/functions/useWindowFocus/index.vue
+++ src/functions/useWindowFocus/index.vue
@@ -1,4 +1,11 @@
 <script setup>
+/*
+useWindowFocus是一个自定义钩子，用于检测当前浏览器窗口是否处于聚焦状态。如果用户正在查看当前的浏览器窗口（即窗口处于活动状态），则该钩子返回true，否则返回false。
+
+在给出的代码中，useWindowFocus函数的返回值被存储在focused变量中。然后，在模板中，通过{{ focused }}将这个值显示出来。如果窗口处于聚焦状态，页面上将显示"Window is focused: true"，否则显示"Window is focused: false"。
+
+这个钩子在需要根据窗口的聚焦状态来改变组件行为的场景中非常有用。例如，你可能希望在用户查看其他窗口时暂停视频播放，或者在用户回到你的应用窗口时刷新一些数据。
+* */
 import { useWindowFocus } from "@vueuse/core";
 const focused = useWindowFocus();
 </script>
